3f1e5e44e782f291f429f2d7b5ac5c0788e9b5dd
Breno Martins <67431981+BrenoMartinsDeOliveiraVasconcelos@users.noreply.github.com>
2021-08-12 16:18:52 -0300

    update

--- linver.py
+++ linver.py
@@ -1,11 +1,16 @@
-import tkinter as tk
+from sys import argv as args
 import distro as ds
 import os
 import platform
 import getpass as gp
 import datetime as dt
 import psutil
-import webbrowser
+if "--clmode" not in args:
+    import tkinter as tk
+    import webbrowser
+# Adicionando um valor placeholder a lista args para caso nn seja passado nenhum argumento
+if len(args) == 1:
+    args.append('--noargs')
 
 
 def github():
@@ -14,8 +19,22 @@ def github():
 
 
 def main():
-    linux = ds.linux_distribution()[0]
-    distrover = ds.linux_distribution()[1]
+    if platform.system() == "Windows":
+        print("You shouldn't run this on Windows, but anyways")
+    linux = ds.name() #ds.linux_distribution()[0]
+    if args[1] == '--noargs':
+        linux = ds.name() #ds.linux_distribution()[0]
+    elif '--distro' in args:
+        try:
+            linux = args[args.index('--distro') + 1].replace('_', ' ').replace('-', '/')
+        except IndexError:
+            linux = ds.name() #ds.linux_distribution()[0]
+        #linux = args[].replace('_', ' ').replace("-", "/")
+    distrover = ds.version() #ds.linux_distribution()[1]
+    codename = ds.codename() #ds.linux_distribution()[2]
+    build = ds.build_number()
+    if build == '0' or build == '':
+        build = 'none'
     year = dt.date.today().year
     kernelver = platform.release()
     fontcolor = '#141414'
@@ -32,14 +51,17 @@ def main():
     disks_list = []
     for disk in disks:
         device = disk.device
-        if device.split("/")[-1][0:-1] == 'loop':
+        if 'loop' in device.split("/")[-1]:
             pass
         else:
             disks_list.append(device)
     partitions = len(disks_list)
     discos = {"sda"}
     for disk in disks_list:
-        discos.add(disk.split("/")[-1][0:-1])
+        disco = disk.split("/")[-1]
+        for i in "1234567890":
+            disco = disco.replace(i, "")
+        discos.add(disco)
 
     # Número de discos
     ndiscos = len(discos)
@@ -48,37 +70,12 @@ def main():
     uptime = dt.datetime.now() - dt.datetime.fromtimestamp(psutil.boot_time())
     uptime = (str(uptime).split('.')[0]).split(":")
 
-    uptimestr = f"{uptime[0]} hours, {uptime[1]} minutes and {uptime[2]} seconds."
+    uptimestr = f"{uptime[0]} hour(s), {uptime[1]} minute(s) and {uptime[2]} second(s)."
 
     script_path = os.path.dirname(os.path.realpath(__file__))
 
-    # Váriabel image como linux.png como placeholder
-    image = f"{script_path}/assets/linux.png"
-
-    # Váriabel root como tkinter.Tk()
-    root = tk.Tk()
-    root.title("Linver")
-    #root.geometry("550x470")
-    root.resizable(False, False)
-    # set background for #ffffff
-    root.configure(background="#ffffff")
-    
-    # Adiciona imagem a janela
-    img = tk.PhotoImage(file=image)
-    imglabel = tk.Label(root, image=img, bg="#ffffff")
-    imglabel.grid(row=0, column=0, columnspan=4, rowspan=2, sticky="nsew")
-
-    # Criar uma divisória abaixo da imagem
-    div = tk.Frame(root, bg="#828282", width=510, height=1)
-    emptydiv = tk.Frame(root, bg="#ffffff", width=20, height=1)
-    emptydiv2 = tk.Frame(root, bg="#ffffff", width=20, height=1)
-    emptydiv.grid(row=2, column=0, columnspan=1, rowspan=2, sticky="nsew")
-    div.grid(row=2, column=1, columnspan=3, rowspan=2, sticky="nsew")
-    emptydiv2.grid(row=2, column=4, columnspan=1, rowspan=2, sticky="nsew")
-
-    # Criar label com o texto
-    label = tk.Label(root, text=f"""{linux} 
-Version {distrover} (Kernel {kernelver})
+    linver = f"""{linux} 
+Version {distrover} {codename} (Build {build}, kernel {kernelver})
 © {year}.
 
 This program is a free and open source product.
@@ -90,24 +87,58 @@ The number of partitions mounted is {partitions} in {ndiscos} disk(s).
 Machine name is {platform.node()} and its uptime is:
 {uptimestr}
 
-{gp.getuser()}
+Thank your for using this program, {gp.getuser()}! ^-^
+    """
+    if '--clmode' not in args:
+        # Váriabel image como linux.png como placeholder
+        image = f"{script_path}/assets/linux.png"
 
-""", bg="#ffffff", fg=fontcolor)
-    label.grid(row=4, column=2)
+        # Aqui muda a imagem de acordo com a distro
+        try:
+            open(f"{script_path}/assets/{linux.replace(' ', '_').replace('/', '-')}.png", 'r')
+            image = f"{script_path}/assets/{linux.replace(' ', '_').replace('/', '-')}.png"
+        except FileNotFoundError:
+            image = f"{script_path}/assets/linux.png"
+
+        # Váriabel root como tkinter.Tk()
+        root = tk.Tk()
+        root.title("Linver")
+        #root.geometry("550x470")
+        root.resizable(False, False)
+        # set background for #ffffff
+        root.configure(background="#ffffff")
+        
+        # Adiciona imagem a janela
+        img = tk.PhotoImage(file=image)
+        imglabel = tk.Label(root, image=img, bg="#ffffff")
+        imglabel.grid(row=0, column=0, columnspan=4, rowspan=2, sticky="nsew")
+
+    # # Criar uma divisória abaixo da imagem
+    # div = tk.Frame(root, bg="#828282", width=510, height=1)
+    # emptydiv = tk.Frame(root, bg="#ffffff", width=20, height=1)
+    # emptydiv2 = tk.Frame(root, bg="#ffffff", width=20, height=1)
+    # emptydiv.grid(row=2, column=0, columnspan=1, rowspan=2, sticky="nsew")
+    # div.grid(row=2, column=1, columnspan=3, rowspan=2, sticky="nsew")
+    # emptydiv2.grid(row=2, column=4, columnspan=1, rowspan=2, sticky="nsew")
+
+        # Criar label com o texto
+        label = tk.Label(root, text=linver, bg="#ffffff", fg=fontcolor)
+        label.grid(row=4, column=2)
 
 
-    # Criar botão de sair
-    exitbutton = tk.Button(root, text="Ok", bg="#ffffff", fg=fontcolor, 
-    command=exit, activebackground="#e0e0e0", borderwidth=1, highlightthickness=0, activeforeground="#000000")
-    exitbutton.grid(row=5, column=2, columnspan=1, sticky="nsew")
+        # Criar botão de sair
+        exitbutton = tk.Button(root, text="Ok", bg="#ffffff", fg=fontcolor, 
+        command=exit, activebackground="#e0e0e0", borderwidth=1, highlightthickness=0, activeforeground="#000000")
+        exitbutton.grid(row=5, column=2, columnspan=1, sticky="nsew")
 
-    # Criar um botão que redireciona para a página do github (https://github.com/BrenoMartinsDeOliveiraVasconcelos/linver)
-    githubbutton = tk.Button(root, text="Github", bg="#ffffff", fg=fontcolor, 
-    command=github, activebackground="#e0e0e0", borderwidth=1, highlightthickness=0, activeforeground="#000000")
-    githubbutton.grid(row=6, column=2, columnspan=1, sticky="nsew")
+        # Criar um botão que redireciona para a página do github (https://github.com/BrenoMartinsDeOliveiraVasconcelos/linver)
+        githubbutton = tk.Button(root, text="Github", bg="#ffffff", fg=fontcolor, 
+        command=github, activebackground="#e0e0e0", borderwidth=1, highlightthickness=0, activeforeground="#000000")
+        githubbutton.grid(row=6, column=2, columnspan=1, sticky="nsew")
 
-    root.mainloop()
-
+        root.mainloop()
+    else:
+        print(linver)
 
 if __name__ == '__main__':
     main()
